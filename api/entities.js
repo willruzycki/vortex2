a792ac1c8a5fab09fd72964b752a2be1ee4b4f3f
willruzycki <william.ruzycki@spartans.ut.edu>
2025-07-22 16:26:44 -0400

    Add files via upload

---
+++ api/entities.js
@@ -0,0 +1,65 @@
+import { base44 } from './base44Client';
+
+
+export const Video = base44.entities.Video;
+
+export const VideoInteraction = base44.entities.VideoInteraction;
+
+export const Follow = base44.entities.Follow;
+
+export const Series = base44.entities.Series;
+
+export const Comment = base44.entities.Comment;
+
+export const Subscription = base44.entities.Subscription;
+
+export const Playlist = base44.entities.Playlist;
+
+export const WatchHistory = base44.entities.WatchHistory;
+
+export const Analytics = base44.entities.Analytics;
+
+export const Notification = base44.entities.Notification;
+
+export const Report = base44.entities.Report;
+
+export const Conversation = base44.entities.Conversation;
+
+export const Message = base44.entities.Message;
+
+export const UserRecommendation = base44.entities.UserRecommendation;
+
+export const SystemMetrics = base44.entities.SystemMetrics;
+
+export const ContentModerationQueue = base44.entities.ContentModerationQueue;
+
+export const UserBehaviorPattern = base44.entities.UserBehaviorPattern;
+
+export const RevenueStream = base44.entities.RevenueStream;
+
+export const SearchAnalytics = base44.entities.SearchAnalytics;
+
+export const ABTest = base44.entities.ABTest;
+
+export const CacheStrategy = base44.entities.CacheStrategy;
+
+export const UserReaction = base44.entities.UserReaction;
+
+export const UserPrivacyConsent = base44.entities.UserPrivacyConsent;
+
+export const DataRetentionPolicy = base44.entities.DataRetentionPolicy;
+
+export const SecurityAuditLog = base44.entities.SecurityAuditLog;
+
+export const PerformanceMetrics = base44.entities.PerformanceMetrics;
+
+export const VideoProcessingQueue = base44.entities.VideoProcessingQueue;
+
+export const RateLimitTracker = base44.entities.RateLimitTracker;
+
+export const SystemHealth = base44.entities.SystemHealth;
+
+
+
+// auth sdk:
+export const User = base44.auth;
